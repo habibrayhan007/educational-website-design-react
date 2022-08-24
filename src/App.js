46c9bc937e0719fdf5462a23f5fac99456c37bd8
Your Name <you@example.com>
2022-08-25 01:59:44 +0600

    pricing and faq section done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/about/About';
 import CourseHome from './components/allcourses/CourseHome';
 import Header from "./components/common/heading/Header"
 import Home from './components/home/Home';
+import Price from './components/pricing/Price';
 import Team from './components/team/Team';
 
 function App () {
@@ -18,6 +19,7 @@ function App () {
         <Route path='/about' element={<About/>}/>
         <Route path='/courses' element={<CourseHome/>}/>
         <Route path='/team' element={<Team/>}/>
+        <Route path='/pricing' element={<Price/>}/>
       </Routes>
     </Router>
       
